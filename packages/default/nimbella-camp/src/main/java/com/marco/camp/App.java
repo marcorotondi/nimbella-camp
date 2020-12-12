d0271e842e87ae42d3b322a58c159f8915d9d01f
Marco Rotondi <rotondi@intre.it>
2020-12-12 18:11:12 +0100

    Init version base

---
+++ packages/default/nimbella-camp/src/main/java/com/marco/camp/App.java
@@ -0,0 +1,13 @@
+package com.marco.camp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!");
+    }
+}
